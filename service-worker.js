a2619a2e84e6caa4172ef37cd95470f1acc4aa8d
the-one-space <kimran0218@gmail.com>
2026-08-24 15:38:13 +0900

    Refresh app cache for contact directory

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "the-one-space-v3";
+const CACHE_NAME = "the-one-space-v4";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=26",
-  "./app.js?v=26",
+  "./style.css?v=27",
+  "./app.js?v=27",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
