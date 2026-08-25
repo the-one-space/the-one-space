a480ba576bac54bd5e5a16b53835ff9e7c38ea7c
the-one-space <kimran0218@gmail.com>
2026-08-25 11:24:27 +0900

    Bump cache for dashboard tagline

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "the-one-space-v11";
+const CACHE_NAME = "the-one-space-v12";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=34",
-  "./app.js?v=34",
+  "./style.css?v=35",
+  "./app.js?v=35",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
