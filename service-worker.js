5cf8c3f6c4adbaeb27c18d30ec5ed75051826911
the-one-space <kimran0218@gmail.com>
2026-08-25 12:01:46 +0900

    Bump cache for recycle bin

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "the-one-space-v18";
+const CACHE_NAME = "the-one-space-v19";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=41",
-  "./app.js?v=41",
+  "./style.css?v=42",
+  "./app.js?v=42",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
