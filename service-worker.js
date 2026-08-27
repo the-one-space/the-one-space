77e7cd5a3009eb5441afcb43319c165a60d96ab7
the-one-space <kimran0218@gmail.com>
2026-08-27 11:15:36 +0900

    Refresh cache for pre-login install

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "the-one-space-v24";
+const CACHE_NAME = "the-one-space-v25";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./style.css?v=44",
-  "./app.js?v=45",
+  "./app.js?v=46",
   "./install-helper.js?v=2",
   "./manifest.json",
   "./icon-192.png",
