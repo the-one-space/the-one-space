360e2dc153764a353a6d5621d028720246d42827
the-one-space <kimran0218@gmail.com>
2026-08-27 11:19:22 +0900

    Refresh cache for administrator controls

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "the-one-space-v25";
+const CACHE_NAME = "the-one-space-v26";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./style.css?v=44",
-  "./app.js?v=46",
+  "./app.js?v=47",
   "./install-helper.js?v=2",
   "./manifest.json",
   "./icon-192.png",
