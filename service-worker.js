514381a6d9c93d918538eab3224fe165f21e20f5
the-one-space <kimran0218@gmail.com>
2026-09-02 10:29:56 +0900

    Refresh PWA cache for recording date input

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "the-one-space-v33";
+const CACHE_NAME = "the-one-space-v34";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./style.css?v=47",
-  "./app.js?v=54",
+  "./app.js?v=55",
   "./install-helper.js?v=2",
   "./manifest.json",
   "./icon-192.png",
