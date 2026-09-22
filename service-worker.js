5ed064a6e35396235f570a4f901cc4cf30533bb5
the-one-space <kimran0218@gmail.com>
2026-09-22 09:32:59 +0900

    Refresh PWA cache for resource owner field

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "the-one-space-v42";
+const CACHE_NAME = "the-one-space-v43";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./style.css?v=47",
-  "./app.js?v=63",
+  "./app.js?v=64",
   "./install-helper.js?v=2",
   "./manifest.json",
   "./icon-192.png",
